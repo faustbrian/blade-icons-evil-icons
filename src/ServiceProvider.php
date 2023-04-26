c9fac84aae0a9062d58414a7e21070be8cfc3ddb
Brian Faust <hello@basecode.sh>
2023-04-26 04:01:17 +0300

    first commit
    Signed-off-by: Brian Faust <hello@basecode.sh>

---
+++ src/ServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BombenProdukt\BladeIcons\EvilIcons;
+
+use BombenProdukt\BladeIcons\Facades\IconFamilyRegistry;
+use BombenProdukt\BladeIcons\IconFamily;
+use BombenProdukt\PackagePowerPack\Package\AbstractServiceProvider;
+
+final class ServiceProvider extends AbstractServiceProvider
+{
+    public function packageRegistered(): void
+    {
+        IconFamilyRegistry::push(
+            IconFamily::fromDirectory('evil', __DIR__ . '/../resources/svg'),
+        );
+    }
+}
